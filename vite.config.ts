fd8b32b71899f1c613f7f96ff6c6eac55bffaca3
TR Akshay <akshaytr19.8@outlook.com>
2025-09-15 21:49:19 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/my-forever",
+  base: "/queen-at-twenty-one/",
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
